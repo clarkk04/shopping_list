5ccc555eee01e5d529880eedc23dab88e391a94b
clarkk04 <clarkk@stu.otc.school.nz>
2026-05-20 09:22:11 +1200

    experimental code

--- shopping_list.py
+++ shopping_list.py
@@ -1,1 +1,19 @@
-print("test")
+def create_file(name):
+    f = open(f"{name}.txt", "x")
+    f.close()
+def delete_file(name):
+    import os
+    if os.path.exists(f"{name}.txt"):
+        os.remove(f"{name}.txt")
+    else:
+        print(f"name does not exist")
+
+test = input("????")
+while test != "0":
+    if test == "1":
+        name = input("name?")
+        create_file(name)
+    else:
+        name = input("name?")
+        delete_file(name)
+    test = input("????")
