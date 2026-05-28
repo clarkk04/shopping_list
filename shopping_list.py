05a9fbc3c2940bd50b9096c7d32f93b6ddfcdb0e
clarkk04 <clarkk@stu.otc.school.nz>
2026-05-28 13:29:23 +1200

    connected to database

--- shopping_list.py
+++ shopping_list.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, g
+import sqlite3
+
+DATABASE = 'shopping_list_database.db'
 
 app = Flask(__name__)
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
 @app.route("/")
 def home():
-    return "LOGIN"
+    sql = "SELECT * FROM user;"
+    results = query_db(sql)
+    return str(results)
 
 def create_file(name):
     f = open(f"{name}.txt", "x")
